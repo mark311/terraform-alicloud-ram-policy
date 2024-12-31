c5acfb66509ef54589259d53844bba49b9d88851
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2024-12-31 14:32:16 +0800

    deprecates variable "policies" and related variables and outputs

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "create" {
 }
 
 variable "policies" {
-  description = "List Map of known policy actions. Each item can include the following field: `name`(the policy name prefix, default to a name with 'terraform-ram-policy-' prefix), `actions`(list of the custom actions used to create a ram policy), `defined_actions`(list of the defined actions used to create a ram policy), `resources`(list of the resources used to create a ram policy, default to [*]), `effect`(Allow or Deny, default to Allow), `force`(whether to delete ram policy forcibly, default to true)."
+  description = "(Deprecated, use alicloud_ram_policy_document data source instead) List Map of known policy actions. Each item can include the following field: `name`(the policy name prefix, default to a name with 'terraform-ram-policy-' prefix), `actions`(list of the custom actions used to create a ram policy), `defined_actions`(list of the defined actions used to create a ram policy), `resources`(list of the resources used to create a ram policy, default to [*]), `effect`(Allow or Deny, default to Allow), `force`(whether to delete ram policy forcibly, default to true)."
   type        = list(map(string))
   default     = []
 }
